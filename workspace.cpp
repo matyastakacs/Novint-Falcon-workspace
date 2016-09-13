dd2174ca4943061aaab4b72ba2153ac3241b7a38
dRacz3 <daniel.racz@irob.uni-obuda.hu>
2016-09-13 12:48:01 +0200

    az egész munkateret kigyüjti Z= 0.65 magasságból

--- workspace.cpp
+++ workspace.cpp
@@ -23,19 +23,17 @@ bool ujsor=false;
 bool isNan=true;
 
 
+for(int y = 0; y < 150; y++)
+{
 
-for(int x = 0; x < 150; x++)
-    {//   qDebug() << " Új X";
-
-        for(int y = 0; y < 150; y++)
-        {
-          //  qDebug() << " Új Y";
+    for(int x = 0; x < 150; x++)
+            {//   qDebug() << " Új X";
 
             for(int z = 0; z < 150; z++)
             {
-            pos[0] = -0.5 + 0.005 * x;
-            pos[1] = -0.5 + 0.005 * y;
-            pos[2] = 0.1 + 0.005 * z;
+            pos[0] = 0 + 0.001 * x;
+            pos[1] = 0 + 0.001 * y;
+            pos[2] = 0.065 + 0.005 * z;
             stamper.IK(angles,pos);
             th[0] = angles.theta1[0];
             th[1] = angles.theta1[1];
@@ -57,22 +55,19 @@ for(int x = 0; x < 150; x++)
             prev_pos = pos;
             }
         }
-
-
     }
-/*
-    for(int x = 0; x < 250; x++)
-    {  // qDebug() << " Új X";
 
-        for(int y = 0; y < 250; y++)
-        {
-         //   qDebug() << " Új Y";
+for(int y = 0; y < 150; y++)
+{
 
-            for(int z = 0; z < 250; z++)
+    for(int x = 0; x < 150; x++)
+            {//   qDebug() << " Új X";
+
+            for(int z = 0; z < 150; z++)
             {
-            pos[0] = -0.5 + 0.005 * x;
-            pos[1] = -0.5 + 0.005 * y;
-            pos[2] = 0.1 - 0.005 * z;
+            pos[0] = 0 - 0.001 * x;
+            pos[1] = 0 - 0.001 * y;
+            pos[2] = 0.065 + 0.005 * z;
             stamper.IK(angles,pos);
             th[0] = angles.theta1[0];
             th[1] = angles.theta1[1];
@@ -94,9 +89,78 @@ for(int x = 0; x < 150; x++)
             prev_pos = pos;
             }
         }
+    }
+
+for(int y = 0; y < 150; y++)
+{
+
+    for(int x = 0; x < 150; x++)
+            {//   qDebug() << " Új X";
+
+            for(int z = 0; z < 150; z++)
+            {
+            pos[0] = 0 - 0.001 * x;
+            pos[1] = 0 + 0.001 * y;
+            pos[2] = 0.065 + 0.005 * z;
+            stamper.IK(angles,pos);
+            th[0] = angles.theta1[0];
+            th[1] = angles.theta1[1];
+            th[2] = angles.theta1[2];
+            loopCount++;
+
+            if(th[0] == th[0] && th[1] == th[1] && th[2] == th[2] && elsoTalalatMegvolt==false){
+                elsoTalalatMegvolt=true;
+                log << pos[0] << " " << pos[1] << " " << pos[2] <<std::endl;
+               // qDebug() << "Első találat a sorban pipa!";
+            }
+
+            if((th[0] != th[0] || th[1] != th[1] || th[2] != th[2]) && elsoTalalatMegvolt==true){
+                log << prev_pos[0] << " " << prev_pos[1] << " " << prev_pos[2] <<std::endl;
+               // qDebug() << "Köv ciklus!";
+                elsoTalalatMegvolt=false;
+                break;}
+
+            prev_pos = pos;
+            }
+        }
+    }
 
 
-    }*/
+
+for(int y = 0; y < 150; y++)
+{
+
+    for(int x = 0; x < 150; x++)
+            {//   qDebug() << " Új X";
+
+            for(int z = 0; z < 150; z++)
+            {
+            pos[0] = 0 + 0.001 * x;
+            pos[1] = 0 - 0.001 * y;
+            pos[2] = 0.065 + 0.005 * z;
+            stamper.IK(angles,pos);
+            th[0] = angles.theta1[0];
+            th[1] = angles.theta1[1];
+            th[2] = angles.theta1[2];
+            loopCount++;
+
+            if(th[0] == th[0] && th[1] == th[1] && th[2] == th[2] && elsoTalalatMegvolt==false){
+                elsoTalalatMegvolt=true;
+                log << pos[0] << " " << pos[1] << " " << pos[2] <<std::endl;
+               // qDebug() << "Első találat a sorban pipa!";
+            }
+
+            if((th[0] != th[0] || th[1] != th[1] || th[2] != th[2]) && elsoTalalatMegvolt==true){
+                log << prev_pos[0] << " " << prev_pos[1] << " " << prev_pos[2] <<std::endl;
+               // qDebug() << "Köv ciklus!";
+                elsoTalalatMegvolt=false;
+                break;}
+
+            prev_pos = pos;
+            }
+        }
+    }
+
     log.close();
     qDebug() << loopCount;
     qDebug() << "KÉSZ";
